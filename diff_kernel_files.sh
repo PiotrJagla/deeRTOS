1d122f47833b853fee23e7ad0721f4f2d3a9f2b2
PiotrJagla <piotrek2002.09@gmail.com>
2024-10-06 12:15:36 +0200

    adding differenciating between kernel files and example kernel files copy

---
+++ diff_kernel_files.sh
@@ -0,0 +1,30 @@
+diff -q ./kernel_files/src/deeRTOS.c ./examples/$1/src/deeRTOS.c
+diff -u <(nl ./kernel_files/src/deeRTOS.c) <(nl ./examples/$1/src/deeRTOS.c)
+
+diff -q ./kernel_files/src/mutex.c ./examples/$1/src/mutex.c
+diff -u <(nl ./kernel_files/src/mutex.c) <(nl ./examples/$1/src/mutex.c)
+
+diff -q ./kernel_files/src/queue.c ./examples/$1/src/queue.c
+diff -u <(nl ./kernel_files/src/queue.c) <(nl ./examples/$1/src/queue.c)
+
+diff -q ./kernel_files/src/semaphore.c ./examples/$1/src/semaphore.c
+diff -u <(nl ./kernel_files/src/semaphore.c) <(nl ./examples/$1/src/semaphore.c)
+
+diff -q ./kernel_files/inc/deeRTOS.h ./examples/$1/inc/deeRTOS.h
+diff -u <(nl ./kernel_files/inc/deeRTOS.h) <(nl ./examples/$1/inc/deeRTOS.h)
+
+diff -q ./kernel_files/inc/mutex.h ./examples/$1/inc/mutex.h
+diff -u <(nl ./kernel_files/inc/mutex.h) <(nl ./examples/$1/inc/mutex.h)
+
+diff -q ./kernel_files/inc/queue.h ./examples/$1/inc/queue.h
+diff -u <(nl ./kernel_files/inc/queue.h) <(nl ./examples/$1/inc/queue.h)
+
+diff -q ./kernel_files/inc/semaphore.h ./examples/$1/inc/semaphore.h
+diff -u <(nl ./kernel_files/inc/semaphore.h) <(nl ./examples/$1/inc/semaphore.h)
+
+diff -q ./portable/$1/portmacro.h ./examples/$1/inc/portmacro.h
+diff -u <(nl ./portable/$1/portmacro.h) <(nl ./examples/$1/inc/portmacro.h)
+
+diff -q ./portable/$1/port.c ./examples/$1/src/port.c
+diff -u <(nl ./portable/$1/port.c) <(nl ./examples/$1/src/port.c)
+
